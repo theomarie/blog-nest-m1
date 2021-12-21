ce85e871d8583b932064d8eaa6008f8a61d7324c
theomarie <theomarie.com>
2021-12-21 15:35:11 +0100

    author_entity

--- src/Entity/author/author.entity.ts
+++ src/Entity/author/author.entity.ts
@@ -1,1 +1,16 @@
-export class AuthorEntity {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class AuthorEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 25 })
+    fullName:string;
+
+    @Column({ length: 25 })
+    email:string;
+
+ 
+}
