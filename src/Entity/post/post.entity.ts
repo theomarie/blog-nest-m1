c78df96c625c83a426bea748a54c979fd7238808
Thomas <>
2021-12-21 15:35:35 +0100

    ajout de post

--- src/Entity/post/post.entity.ts
+++ src/Entity/post/post.entity.ts
@@ -1,1 +1,23 @@
-export class PostEntity {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToOne } from 'typeorm';
+import { AuthorEntity } from '../author/author.entity';
+
+@Entity()
+export class PostEntity {
+   @PrimaryGeneratedColumn()
+   id: number;
+
+   @Column({length: 50})
+   title: string;
+
+   @Column({length: 50})
+   subtitle: string;
+   
+   @Column()
+   image: string;
+
+   @Column()
+   date: Date;
+
+//    @OneToOne(type => AuthorEntity, author => author.id)
+//     balance: AuthorEntity;
+}
